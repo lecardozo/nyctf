40dbcb2e35b7c912c1a1912496d43cb470a2f617
Lucas Cardozo <lucas.cardozo@nexadigital.com.br>
2021-02-06 15:20:14 -0300

    packaging the model

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+SERVER_DEPENDENCIES = [
+    "flask", "flasgger", "marshmallow"
+]
+
+setup(
+    name="nyctfp",
+    version="0.0.1",
+    packages=find_packages(exclude=["test*"]),
+    install_requires=[
+        "pandas==1.1.4",
+        "scikit-learn==0.24.1",
+        "pyarrow==3.0.0",
+        "cloudpickle==1.6.0",
+        "xgboost==1.3.3",
+        "smart_open",
+        "boto3"
+    ]
+    extras_require={
+        "server": SERVER_DEPENDENCIES,
+    },
+    entry_points={"console_scripts": ["mlcli=model.cli:cli"]}
+)
