a6f38aa3f109c0794f2de82cf228cb352af8f894
Lucas Cardozo <lucas.cardozo@nexadigital.com.br>
2021-02-06 14:24:25 -0300

    adding training and preprocessing related functions

---
+++ model/training/features.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+
+def haversine_distance(lon1: pd.Series, lat1: pd.Series, lon2: pd.Series, lat2: pd.Series):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.
+    
+    Taken from: https://stackoverflow.com/questions/29545704/fast-haversine-approximation-python-pandas
+    """
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+    return km
+
+def preprocess(data):
+    data["pickup_datetime"] = pd.to_datetime(data["pickup_datetime"])
+    data["hav_distance_km"] = haversine_distance(
+        data["pickup_longitude"],
+        data["pickup_latitude"],
+        data["dropoff_longitude"],
+        data["dropoff_latitude"]
+    )
+    data["day_of_week"] = data["pickup_datetime"].dt.dayofweek
+    data["hour"] = data["pickup_datetime"].dt.hour
+    return data
+
+def spark_preprocess(df):
+    from pyspark.sql.function import pandas_udf, dayofweek, hour
+    from pyspark.sql.types import LongType
+
+    haversine_distance_udf = pandas_udf(haversine_distance, returnType=LongType())
+
+    df = df.withColumn(
+        "hav_distance_km", 
+        haversine_distance_udf(
+            df.pickup_longitude, df.pickup_latitude, df.dropoff_longitude, df.dropoff_latitude
+        )
+    )
+
+    # Here we need to adjust dayofweek to keep the same Pandas pattern (0-Mon, 1-Tue ... 6-Sun)
+    df = (
+        df.withColumn("day_of_week", dayofweek(df.pickup_datetime) - 2)
+          .withColumn("day_of_week", when(col("day_of_week") == -1, 6)
+                                     .otherwise(col("day_of_week"))
+          )
+    )
+    df = df.withColumn("hour", hour(df.pickup_datetime))
+
+    return df
---
+++ model/training/trainer.py
@@ -0,0 +1,67 @@
+import cloudpickle
+import pandas as pd
+from pathlib import Path
+from sklearn.model_selection import train_test_split, GridSearchCV, PredefinedSplit
+from xgboost import XGBRegressor
+from .features import preprocess
+
+RANDOM_SEED = 1234
+SAMPLE_SIZE = 5542385 # roughtly 10% of the data
+
+def split_dataset(dataset_path: str):
+    dirname = Path(dataset_path).parent
+    if (Path(dirname, "training-set.parquet").exists() and
+        Path(dirname, "validation-set.parquet").exists()):
+        print("Reading pre-split datasets")
+        training = pd.read_parquet("data/training-set.parquet")
+        validation = pd.read_parquet("data/validation-set.parquet")
+    else:
+        print("Splitting dataset")
+        data = pd.read_csv(dataset_path, nrows=SAMPLE_SIZE, parse_dates=["pickup_datetime"])
+        training, validation = train_test_split(data, random_state=RANDOM_SEED)
+        training.to_parquet("data/training-set.parquet")
+        validation.to_parquet("data/validation-set.parquet")
+    
+    return training, validation
+
+
+def clean_dataset(df):
+    print("Cleaning dataset")
+    invalid_fare = (df.fare_amount <= 2.5)
+    invalid_coordinates = (
+        df.filter(regex="lon|lat").eq(0).any(axis=1) |
+        df.filter(regex="lon|lat").isnull().any(axis=1) |
+        df.filter(regex="lon").abs().gt(180).any(axis=1) |
+        df.filter(regex="lat").abs().gt(90).any(axis=1)
+    )
+    invalid_passenger_count = df.passenger_count > 6
+    
+    df = df[~(
+        invalid_coordinates | invalid_fare | invalid_passenger_count
+    )]
+    return df
+
+
+def run(training_data: pd.DataFrame, validation_data: pd.DataFrame,
+        artifact_path: str):
+    training = training_data.assign(split=-1)
+    validation = validation_data.assign(split=0)
+    full_data = pd.concat([training, validation])
+    full_data = clean_dataset(full_data)
+    splits = PredefinedSplit(test_fold=full_data.split)
+    param_space = {
+        "max_depth": [3, 4],
+        "n_estimators": [50, 100]
+    }
+    estimator = XGBRegressor()
+    model = GridSearchCV(estimator, cv=splits, param_grid=param_space, refit=True)
+    print("Generating features")
+    processed_data = preprocess(full_data)
+    print("Training model")
+    model.fit(
+        X=processed_data[["hav_distance_km", "day_of_week", "hour"]], 
+        y=processed_data[["fare_amount"]]
+    )
+
+    with open(artifact_path, "wb") as out:
+        cloudpickle.dump(model, out)
