9b4e5521a0a42b2810d6401f54b902c91c3e7a83
Lucas Cardozo <lucas.cardozo@nexadigital.com.br>
2021-02-08 17:21:05 -0300

    fix pyarrow version to avoid version mismatching bugs

--- scripts/install-model-package.sh
+++ scripts/install-model-package.sh
@@ -4,3 +4,4 @@ aws s3 cp $1 package.tar.gz
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install --upgrade setuptools
 sudo python3 -m pip install package.tar.gz
+sudo python3 -m pip install --upgrade pyarrow==0.15.1
